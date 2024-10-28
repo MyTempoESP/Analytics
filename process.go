a7a22db31782a595a1c38855275e8530ea11575e
TsukiGva2 <tsukigva@gmail.com>
2024-10-28 17:30:35 -0300

    ffcom

--- process.go
+++ process.go
@@ -14,7 +14,51 @@ func (a *Ay) AtualizarTagsUnicas(tagsUnicas int64) {
 	}
 }
 
-func (a *Ay) AtualizarTags(tags int64) {
+func (a *Ay) AtualizarTags(tags int64) (ok bool) {
+
+	var totalAnterior int64 = 0
+
+	ok = true
+
+	res, err := a.db.Query("SELECT tags_total FROM stats")
+
+	if err != nil {
+
+		log.Println("(AtualizarTags)", err)
+
+		return
+	}
+
+	defer res.Close()
+
+	if res.Next() {
+
+		err = res.Scan(&totalAnterior)
+
+		if err != nil {
+
+			log.Println("(AtualizarTags)", err)
+
+			return
+		}
+	}
+
+	if totalAnterior == 0 {
+
+		_, err = a.db.Exec(`REPLACE INTO stats (tags_total) VALUES (1) WHERE id = 1`)
+
+		ok = false
+
+		return
+	}
+
+	_, err = a.db.Exec(`REPLACE INTO stats (tags_total) VALUES (?) WHERE id = 1`, tags)
+
+	if err != nil {
+		log.Println("(AtualizarTags)", err)
+	}
+
+	return
 }
 
 func (a *Ay) Process() {
@@ -52,7 +96,14 @@ func (a *Ay) Process() {
 		for {
 			<-atualizaContagem.C
 
-			a.AtualizarTags(atomic.LoadInt64(&tags))
+			/*
+				true  => prosseguir
+				false => resetar
+			*/
+			if !a.AtualizarTags(atomic.LoadInt64(&tags)) {
+				tagSet.Clear()
+				atomic.StoreInt64(&tags, 0)
+			}
 		}
 	}()
 
