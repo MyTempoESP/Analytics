6ca239e74b8bd5c63c388af29f529a56669f74f8
TsukiGva2 <tsukigva@gmail.com>
2024-11-21 16:35:31 -0300

    JADDBABEDH

--- process.go
+++ process.go
@@ -38,11 +38,52 @@ const (
 	UPDATE
 		tags_unicas = ?
 	`
+
+	QUERY_ATUALIZAR_ANTENAS = `
+	INSERT INTO stats (
+	    id,
+	    tags_unicas,
+	    tags_total,
+	    Antena1,
+	    Antena2,
+	    Antena3,
+	    Antena4
+	)
+	VALUES(
+		1,
+		0,
+		0,
+		0,
+		0,
+		0,
+		0
+	)
+	ON DUPLICATE KEY
+	UPDATE
+		Antena1 = ?,
+		Antena2 = ?,
+		Antena3 = ?,
+		Antena4 = ?
+	`
 )
 
 func (a *Ay) AtualizarTagsUnicas(tagsUnicas int64) {
 
 	_, err := a.db.Exec(QUERY_ATUALIZAR_TAGS_UNICAS, tagsUnicas)
+
+	if err != nil {
+		log.Println("(AtualizarTagsUnicas)", err)
+	}
+}
+
+func (a *Ay) AtualizarAntenas(antenas *[4]int64) {
+
+	_, err := a.db.Exec(QUERY_ATUALIZAR_ANTENAS,
+		atomic.LoadInt64(&antenas[0]),
+		atomic.LoadInt64(&antenas[1]),
+		atomic.LoadInt64(&antenas[2]),
+		atomic.LoadInt64(&antenas[3]),
+	)
 
 	if err != nil {
 		log.Println("(AtualizarTagsUnicas)", err)
@@ -109,7 +150,11 @@ func (a *Ay) AtualizarTags(tags int64) (ok bool) {
 
 func (a *Ay) Process() {
 
-	var tags int64 /* shared */
+	var (
+		tags int64 /* shared */
+
+		antennas [4]int64
+	)
 
 	tagSet := NewIntSet()
 
@@ -123,6 +168,8 @@ func (a *Ay) Process() {
 
 				continue
 			}
+
+			atomic.AddInt64(&antennas[t.Antena%4], 1)
 
 			atomic.AddInt64(&tags, 1)
 
@@ -149,9 +196,18 @@ func (a *Ay) Process() {
 			log.Println("Atualizando tags")
 
 			if !a.AtualizarTags(atomic.LoadInt64(&tags)) {
+
 				tagSet.Clear()
+
 				atomic.StoreInt64(&tags, 0)
+
+				antennas[0] = 0
+				antennas[1] = 0
+				antennas[2] = 0
+				antennas[3] = 0
 			}
+
+			a.AtualizarAntenas(&antennas)
 		}
 	}()
 
