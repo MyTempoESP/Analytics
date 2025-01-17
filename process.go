3fcff83a403c2347a0d3b9f1ec041b57a085d179
TsukiGva2 <tsukigva@gmail.com>
2025-01-17 12:32:21 -0300

    AAGFHHFHHF

--- process.go
+++ process.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"log"
-	"os"
 	"sync/atomic"
 	"time"
 
 	"analytics/intSet"
-	"analytics/lcdlogger"
-	"github.com/MyTempoesp/flick"
-	"github.com/prometheus-community/pro-bing"
 )
 
 const (
@@ -214,109 +210,4 @@ func (a *Ay) Process() {
 			a.AtualizarAntenas(&antennas)
 		}
 	}()
-
-	var readerIP = os.Getenv("READER_IP")
-	var readerOctets = lcdlogger.IPIfy(readerIP)
-
-	var readerState atomic.Bool
-	var readerPing atomic.Int64
-
-	display, displayErr := lcdlogger.NewSerialDisplay()
-
-	/* > Monitoring can be skipped if NewSerialDisplay() errors out, disabling the routine in Line 221 */
-	if displayErr != nil {
-
-		goto skip_monitoring
-	}
-
-	go func() {
-
-		p, err := probing.NewPinger(readerIP)
-
-		//p.SetPrivileged(true)
-
-		if err != nil {
-
-			return
-		}
-
-		p.Count = 0xFFFE
-		p.Interval = 4 * time.Second
-
-		p.OnSend = func(pkt *probing.Packet) {
-
-			log.Printf("IP Addr: %s\n", pkt.IPAddr)
-
-			readerState.Store(false)
-		}
-
-		p.OnRecv = func(pkt *probing.Packet) {
-
-			log.Printf("IP Addr: %s receive, RTT: %v\n", pkt.IPAddr, pkt.Rtt)
-
-			readerState.Store(true)
-			readerPing.Store(pkt.Rtt.Milliseconds())
-		}
-
-		p.Run()
-	}()
-
-	go func() {
-
-		const NUM_EQUIP = 701
-
-		for {
-
-			commVerif := flick.WEB
-
-			switch display.Screen {
-			case lcdlogger.SCREEN_TAGS:
-				display.ScreenTags(
-					NUM_EQUIP,
-					commVerif,
-					/* Tags */ tags.Load(),
-					/* Atletas */ tagSet.Count(),
-				)
-			case lcdlogger.SCREEN_ADDR:
-
-				ok := flick.OK
-
-				if !readerState.Load() {
-
-					ok = flick.DESLIGAD
-				}
-
-				display.ScreenAddr(
-					NUM_EQUIP,
-					commVerif,
-					readerPing.Load(),
-					/* IP */ readerOctets,
-					/* leitor OK? */ ok,
-				)
-			case lcdlogger.SCREEN_WIFI:
-				display.ScreenWifi(
-					NUM_EQUIP,
-					commVerif,
-					/* WIFI */ flick.CONECTAD,
-					/* 4G */ flick.DESLIGAD,
-				)
-			case lcdlogger.SCREEN_STAT:
-				display.ScreenStat(
-					NUM_EQUIP,
-					commVerif,
-					lcdlogger.ToForthNumber(antennas[0].Load()),
-					lcdlogger.ToForthNumber(antennas[1].Load()),
-					lcdlogger.ToForthNumber(antennas[2].Load()),
-					lcdlogger.ToForthNumber(antennas[3].Load()),
-				)
-			}
-
-			display.SwitchScreens()
-
-			time.Sleep(100 * time.Millisecond)
-		}
-	}()
-
-skip_monitoring:
-	select {}
 }
